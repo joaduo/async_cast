aba277d10536803396583d365d897dad3c1b234d
joaduo <joaduo@gmail.com>
2023-12-19 06:33:39 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def long_description():
 setup(
   name = name,
   py_modules=[name],
-  version = '0.4.2',
+  version = '0.6.0',
   description = 'Cast an async function to blocking function and viceversa. Also use threads pools if needed.',
   long_description=long_description(),
   long_description_content_type='text/x-rst',
